107eb61bcce4eb3fe4492946d49d4b3f0a65a49e
Vedant Patil <97403812+Vedant515@users.noreply.github.com>
2025-07-31 16:29:22 +0530

    Delete EMP TRACKER directory

--- EMP TRACKER/models/Company.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require('mongoose');
-
-const CompanySchema = new mongoose.Schema({
-  companyId: { type: String, required: true, unique: true },
-  createdBy: { type: String, required: true } 
-});
- 
-module.exports = mongoose.model("Company", CompanySchema);
